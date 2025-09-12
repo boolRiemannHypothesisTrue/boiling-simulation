d96eb3cbb293531c50f2f7fe95d61ea81237a2db
Mark <boolRiemannHypothesisTrue@hotmail.com>
2025-09-12 17:09:14 +0300

    energy derivative update

--- Derivatives of residual functions/Energy/QT_dQT_dux.m
+++ Derivatives of residual functions/Energy/QT_dQT_dux.m
@@ -17,9 +17,10 @@ function val = QT_dQT_dux(QT, fields,params,phase,dx,dy)
     [M, N] = size(QT);
     val = zeros(M, N);
 
+    
     % Внутренние узлы
     i = 3:M-2; j = 3:N-2;
-    
+
     val(i,j) = QT(i,j) * rho(i,j) * cp(i,j) *  ( T(i+1,j) - T(i-1,j) ) / (2*dx) - ...
         QT(i-1,j) * (8/3) * mu(i-1,j) / (2*dx) * ( (u_x(i,j) - u_x(i-2,j) ) / (2*dx) + ...
         ( u_y(i-1,j+1) - u_y(i-1,j-1) ) / (2*dy) ) + ...
@@ -31,17 +32,17 @@ function val = QT_dQT_dux(QT, fields,params,phase,dx,dy)
         QT(i+1,j) * (16/3) * mu(i+1,j) * ( u_y(i+1,j+1) - u_y(i+1,j-1) ) / (2*dx*2*dy) - ...
         QT(i,j-1) * 2 * mu(i,j-1) / (2*dy) * ( ( u_y(i+1,j-1) - u_y(i-1,j-1) ) / (2*dx) + ( u_x(i,j) - u_x(i,j-2) ) / (2*dy) ) + ...
         QT(i,j-1) * 2 * mu(i,j+1) / (2*dy) * ( ( u_y(i+1,j+1) - u_y(i-1,j+1) ) / (2*dx) + ( u_x(i,j+2) - u_x(i,j) ) / (2*dy) ) ; 
-    
+
         % горизонтальные линии
     [i1, j1] = ndgrid([2, M-1], 2:N-1);
-    
+
     % вертикальные линии
     [i2, j2] = ndgrid(2:M-1, [2, N-1]);
-    
+
     % объединяем
     i = [i1(:); i2(:)];
     j = [j1(:); j2(:)];
-    
+
     val(i,j) = QT(i,j) * rho(i,j) * cp(i,j) *  ( T(i+1,j) - T(i-1,j) ) / (2*dx) - ...
         QT(i-1,j) * (16/3) * mu(i-1,j) * ( u_y(i-1,j+1) - u_y(i-1,j-1) ) / (2*dx*2*dy) - ...
         QT(i+1,j) * (16/3) * mu(i+1,j) * ( u_y(i+1,j+1) - u_y(i+1,j-1) ) / (2*dx*2*dy);
